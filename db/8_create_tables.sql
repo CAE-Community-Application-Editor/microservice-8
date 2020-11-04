17ea4e67cd9811843c4b107debe4fde84d3ed803
CAEUser <6@dy[hYD>
2020-11-04 19:52:30 +0000

    init microservice

---
+++ db/8_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  whiteboard
+-- Automatically generated sql script for the service 8, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table page.
+--
+CREATE TABLE whiteboard.page (
+  content text ,
+  id int ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
